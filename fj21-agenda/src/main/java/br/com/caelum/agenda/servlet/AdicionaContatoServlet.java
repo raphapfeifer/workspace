32be7d9217ba91be06d5c4150471b7db2920634e
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2017-04-19 01:02:34 -0300

    19/04/2017

---
+++ fj21-agenda/src/main/java/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
@@ -0,0 +1,49 @@
+package br.com.caelum.agenda.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/adicionaContato")
+public class AdicionaContatoServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		String nome = request.getParameter("nome");
+		String endereco = request.getParameter("endereco");
+		String email = request.getParameter("email");
+		String dataEmTexto = request.getParameter("dataNascimento");
+		Calendar dataNascimento = null;
+		
+		try{
+			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(date);
+		}catch(ParseException e){
+			out.println("Erro de conversão da data");
+			return;
+			}
+
+		
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Contato foi inserido!");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+
+}
