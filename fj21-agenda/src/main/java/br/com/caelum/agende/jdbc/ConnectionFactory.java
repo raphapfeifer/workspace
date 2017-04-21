b939e58680275e42118777bdf810797afca6f99b
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2017-04-20 23:33:50 -0300

    19/04/2017

--- fj21-agenda/src/main/java/br/com/caelum/agende/jdbc/ConnectionFactory.java
+++ fj21-agenda/src/main/java/br/com/caelum/agende/jdbc/ConnectionFactory.java
@@ -8,6 +8,7 @@ public class ConnectionFactory {
 
 	public Connection getConnection(){
 		try{
+			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 			return DriverManager.getConnection(
 					"jdbc:mysql://localhost/fj21", "root","1234");
 		}catch(SQLException e){
